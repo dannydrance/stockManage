27ad64850cf53885d1d5fad65201a26ddf0807f5
HAKIZAYEZU Daniel <90722447+dannydrance@users.noreply.github.com>
2025-02-27 15:03:55 +0200

    New update

--- gisele/celery.py
+++ gisele/celery.py
@@ -1,16 +1,9 @@
-from __future__ import absolute_import, unicode_literals
-import os
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gisele.settings')
-
 app = Celery('gisele')
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related config keys should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+@app.task
+def send_daily_email_task():
+    from stoke.views import send_daily_report
+    send_daily_report()
--- stoke/tasks.py
+++ stoke/tasks.py
@@ -1,7 +1,14 @@
-from celery import shared_task
-from django.conf import settings
-from views import send_daily_report
+from celery.schedules import crontab
+from gisele.celery import app
+from stoke.views import send_daily_report
 
-@shared_task
-def send_daily_email():
+@app.task
+def send_daily_email_task():
     send_daily_report()
+
+app.conf.beat_schedule = {
+    'send-daily-email-every-morning': {
+        'task': 'stoke.tasks.send_daily_email_task',
+        'schedule': crontab(hour=0, minute=15),
+    },
+}
