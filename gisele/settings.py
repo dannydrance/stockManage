1ed7f3661db7c3a96c109cd9648880d45804703e
HAKIZAYEZU Daniel <90722447+dannydrance@users.noreply.github.com>
2025-03-06 17:47:01 +0200

    New Update

--- gisele/settings.py
+++ gisele/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7u!*2kzmw&zu60st*s__gu@88-pxihvurac00#2s8!qeonltm6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
