e1b5bea93c37162834eb76231703d408e7f81fb3
dannydrance <hakizayezudaniel.com>
2025-03-20 09:28:01 +0200

    New update

--- stoke/admin.py
+++ stoke/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Product, SoledProduct
 
+class ProductCarAdmin(admin.ModelAdmin):
+    list_display = ('card_id', 'category', 'name', 'product_number', 'produced_at', 'expired_on')
+    search_fields = ('card_id', 'category', 'name')
+    list_filter = ('category',)
+
+class SoledProductCarAdmin(admin.ModelAdmin):
+    list_display = ('card_id', 'name', 'sold_number')
+    search_fields = ('card_id', 'name')
+    #list_filter = ('category',)
+
 # Register your models here.
-admin.site.register(Product)
-#admin.site.register(SoledProduct)
+admin.site.register(Product, ProductCarAdmin)
+admin.site.register(SoledProduct, SoledProductCarAdmin)
